6f528d3ecb2e159bf9242be01f547bce2627ef81
Tom Groenwoldt <tom.groenwoldt@elokon.com>
2024-11-11 13:01:16 +0100

    Use http-types that uses futures-util 2.5.0

--- src/handshake.rs
+++ src/handshake.rs
@@ -1,5 +1,5 @@
 /// Upgrade an HTTP connection into an SSE session.
-pub fn upgrade(headers: &mut impl AsMut<http_types::Headers>) {
+pub fn upgrade(headers: &mut impl AsMut<http_types::headers::Headers>) {
     let headers = headers.as_mut();
     headers.insert("Cache-Control", "no-cache");
     headers.insert("Content-Type", "text/event-stream");
